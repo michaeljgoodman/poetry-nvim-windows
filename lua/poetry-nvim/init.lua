826191e5aafa47783c27b8db046666f5c107c298
Michael Goodman <michael.goodman@controlrisks.com>
2025-11-26 00:13:05 +0000

    Altered to work for Windows

--- lua/poetry-nvim/init.lua
+++ lua/poetry-nvim/init.lua
@@ -1,24 +1,25 @@
 local M = {}
 
-local checkForLockfile = function()
-  local match = vim.fn.glob(vim.fn.getcwd() .. "/poetry.lock")
-  if match ~= "" then
-    local poetry_venv = vim.fn.trim(vim.fn.system("poetry env info -p"))
-    local path_extended = string.format("%s/bin:%s", poetry_venv, vim.env.PATH)
-    vim.env.VIRTUAL_ENV = poetry_venv
-    vim.env.PATH = path_extended
-  end
+local function checkForLockfile()
+	local cwd = vim.fn.getcwd()
+	local lockfile = cwd .. "\\poetry.lock"
+
+	if vim.fn.filereadable(lockfile) == 1 then
+		local poetry_venv = vim.fn.trim(vim.fn.system("poetry env info -p"))
+		if poetry_venv ~= "" then
+			local scripts_path = poetry_venv .. "\\Scripts"
+			local path_extended = scripts_path .. ";" .. vim.env.PATH
+			vim.env.VIRTUAL_ENV = poetry_venv
+			vim.env.PATH = path_extended
+		end
+	end
 end
 
 M.setup = function()
-  -- run on startup
-  checkForLockfile()
-  -- and when changing directory
-  vim.api.nvim_create_autocmd({ "DirChanged" }, {
-    callback = function()
-      checkForLockfile()
-    end,
-  })
+	checkForLockfile()
+	vim.api.nvim_create_autocmd("DirChanged", {
+		callback = checkForLockfile,
+	})
 end
 
 return M
